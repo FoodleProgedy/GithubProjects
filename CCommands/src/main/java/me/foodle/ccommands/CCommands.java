0007c644498372901235f373c5e761a2c1c28903
BuildTools <unconfigured@null.spigotmc.org>
2017-07-10 19:46:39 +0200

    Initial commit

---
+++ CCommands/src/main/java/me/foodle/ccommands/CCommands.java
@@ -0,0 +1,26 @@
+package me.foodle.ccommands;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CCommands extends JavaPlugin implements Listener{
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this,this);
+    }
+    @EventHandler
+    public void command(PlayerCommandPreprocessEvent e){
+        for(String s : getConfig().getKeys(false)){
+            if(e.getMessage().equalsIgnoreCase("/"+s)) {
+                for(String msg : getConfig().getStringList(s+".messages")) {
+                    e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',msg));
+                }
+            }
+        }
+    }
+}
